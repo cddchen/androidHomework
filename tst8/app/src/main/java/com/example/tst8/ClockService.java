37724e1e94e7e62e1e3baa82b93ba16e083bee30
cddchen <1354903898@qq.com>
2019-12-21 10:16:49 +0800

    第八次实验第一个作业

---
+++ tst8/app/src/main/java/com/example/tst8/ClockService.java
@@ -0,0 +1,30 @@
+package com.example.tst8;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ClockService extends Service {
+    public ClockService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
